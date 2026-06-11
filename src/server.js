eb45817aacdb4dc73621b8812f97a44ae9651a03
sohaib ODS <sohaibnawaz2000@gmail.com>
2026-06-11 22:02:57 +0500

    handlers created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import connectDB from "./db/db_connection.js";
 import dotenv from "dotenv"
+import app from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log("DB connection failed!", err);
+})
